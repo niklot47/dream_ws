28ca3e51e30a976608b7fa6bb3f4499247ebce18
Niklot47 <niklot47@gmail.com>
2024-08-30 11:04:33 +0000

    kr600 added

---
+++ build/kr600/CMakeFiles/kr600_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kr600_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/kr600_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
