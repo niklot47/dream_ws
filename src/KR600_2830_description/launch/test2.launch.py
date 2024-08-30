9af34f90e565f31ef4912f6f29852cc64a9b2f11
niklot47 <77785451+niklot47@users.noreply.github.com>
2024-08-30 10:35:24 +0200

    Update test2.launch.py

--- src/KR600_2830_description/launch/test2.launch.py
+++ src/KR600_2830_description/launch/test2.launch.py
@@ -3,7 +3,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 
 def generate_launch_description():
 
