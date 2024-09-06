d7345b34524cf43a6f81d27cc119ce687584d74d
niklot47 <77785451+niklot47@users.noreply.github.com>
2024-09-06 09:50:14 +0200

    Delete build directory

--- build/kr600/CMakeFiles/kr600_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/kr600_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/kr600_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/kr600_gazebo/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/root/dream_ws/install/kr600/share/kr600/package.sh"
